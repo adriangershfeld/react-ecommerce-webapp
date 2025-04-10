d8cd530230ae53736c2b7ff4c4c1ba281ba49a29
adriangershfeld <adriangershfeld@protonmail.com>
2025-04-10 18:51:58 -0500

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
+// Import configureStore from Redux Toolkit to create the Redux store
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './cartSlice';
 
 // Redux store configuration using Redux Toolkit
 export const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
+    cart: cartReducer 
+  } // 'cart' becomes this slice's key in the state object + determines how data is accessed in state tree (state.cart)
 });
